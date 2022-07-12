844a26a6334c9ace0b5181b53180fdc4adf59638
ckhowell <ckhowell@me.com>
2022-07-12 10:30:10 +1000

    toml

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,9 +1,11 @@
 // purgecss.config.js
 
+const jekyllEnv = process.env.JEKYLL_ENV || "development";
+
 module.exports = {
     // These are the files that Purgecss will search through
-    content: ["./_site/**/*.html"],
+    content: ["_site/**/*.html"],
   
     // These are the stylesheets that will be subjected to the purge
-    css: ["./_site/assets/css/*.css"] 
+    css: ["_site/assets/css/*.css"] 
   };
