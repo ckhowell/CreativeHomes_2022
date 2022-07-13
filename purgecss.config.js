d314bf8d9cd2afaf0fd9abe7efa9d59c9f7aa89b
ckhowell <ckhowell@me.com>
2022-07-13 15:48:29 +1000

    hero

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,9 +3,8 @@ const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
     // These are the files that Purgecss will search through
-    content: ["./_site/**/*.html", "./_site/**/*.js"],
+    content: ["_site/**/*.html", "_site/**/*.js"],
     // These are the stylesheets that will be subjected to the purge
     css: ["./assets/css/**/*.css"],
     output: ["_site/assets/css/"],
-    whitelistPatterns: [/tns-[a-zA-Z-]*/, /carousel[a-zA-Z-]*/ ],
   };
