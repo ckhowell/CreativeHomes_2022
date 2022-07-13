4f7b62317245a5a2c295d6c0187fe92b3eb27586
ckhowell <ckhowell@me.com>
2022-07-13 12:10:30 +1000

    optimisation

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,10 +1,10 @@
-// const jekyllEnv = process.env.JEKYLL_ENV || "development";
+const jekyllEnv = process.env.JEKYLL_ENV || "development";
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
     // These are the files that Purgecss will search through
     content: ["./_site/**/*.html"],
-  
     // These are the stylesheets that will be subjected to the purge
-
-    css: ["./_site/assets/css/index.css"] 
+    css: ["./assets/css/**/*.css"],
+    output: ["_site/assets/css/"]
   };
